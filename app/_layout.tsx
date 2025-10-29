cce2520ebd93c72c8d10faec0418713b7a54cc4c
Chukastur <fernandolcmusic@hotmail.com>
2025-10-30 00:42:42 +0100

    FIN DE SECCION 10 DEL CURSO DE REACT NATIVE

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,49 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { allRoutes } from "@/constants/Routes";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { useThemeColor } from "@/hooks/use-theme-color";
+import "../global.css";
 
 export default function RootLayout() {
+  const backgroundColor = useThemeColor({}, "background");
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
+    <GestureHandlerRootView
+      style={{ backgroundColor: "backgroundColor", flex: 1 }}
+    >
       <StatusBar style="auto" />
-    </ThemeProvider>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack
+          screenOptions={{
+            headerShadowVisible: false,
+            contentStyle: { backgroundColor: backgroundColor },
+            headerStyle: { backgroundColor: backgroundColor },
+          }}
+        />
+        <Stack.Screen
+          options={{
+            title: "",
+          }}
+        />
+        {allRoutes.map((route) => (
+          <Stack.Screen
+            key={route.name}
+            options={{
+              title: route.title,
+            }}
+          />
+        ))}
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
